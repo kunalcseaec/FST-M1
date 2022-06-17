654306c3ed31d5878909b82b70b69779158d9fb3
kunalcseaec <58936968+kunalcseaec@users.noreply.github.com>
2022-06-17 13:47:23 +0530

    Delete activity2.py

--- activity2.py
+++
@@ -1,9 +0,0 @@
-from xml.sax import xmlreader
-
-
-x = int(input("Please Enter a number: "))
-y = x % 2
-if y > 0:
-    print("Number is an odd number.")
-else:
-    print("Number is  an even number.")
