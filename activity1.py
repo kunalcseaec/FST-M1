02130cdf2e955d5fc0c994d3a12d93555bbb8e38
kunalcseaec <58936968+kunalcseaec@users.noreply.github.com>
2022-06-17 13:43:42 +0530

    Delete activity1.py

--- activity1.py
+++
@@ -1,4 +0,0 @@
-name = input( "What is your name: " )
-age = int( input( "How old are you: " ) )
-year = str( ( 2020 - age ) + 100 )
-print( name + " the year that you will turn 100 years old. " + year )
